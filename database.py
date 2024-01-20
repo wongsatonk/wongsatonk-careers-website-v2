d66500ab3d9840785e225b10bbefa25852eb4018
wongsatonk <128725386+wongsatonk@users.noreply.github.com>
2024-01-20 20:05:12 +0000

    dynamic page for each job

--- database.py
+++ database.py
@@ -29,11 +29,21 @@ def load_jobs_from_db():
     jobs = [] 
     # print(results[0]._mapping)
     for row in results:
-      jobs.append(row._mapping)
+      jobs.append(dict(row._mapping))
       # print(row._mapping)
-  
     # print(result_dicts)
   return jobs
 
+def load_job_from_db(id):
+  with engine.connect() as conn:
+    result = conn.execute(text("select * from jobs where id = :val"), {"val": id})
 
-print(load_jobs_from_db())
+    rows = result.all()
+    if len(rows) == 0:
+      return None
+    else:
+      return dict(rows[0]._mapping)
+
+# print(load_job_from_db(1))
+
+# print(load_jobs_from_db())
